43b4a11a4bb69aff578f2688028292b5aabb14f0
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-27 22:07:39 +0300

    Add config for shadowIOS

---
+++ config/shadowIOS.js
@@ -0,0 +1,10 @@
+// Config for Shadow Prop Types IOS
+var shadowIOS = {
+  shC: 'shadowColor',
+  shOff: 'shadowOffset',
+  shOp: 'shadowOpacity',
+  shR: 'shadowRadius'
+};
+
+module.exports = shadowIOS;
+
