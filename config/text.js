ace5dcd03fc0def24c2c985f2b223f4842e57e26
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-27 22:07:49 +0300

    Add config for text

---
+++ config/text.js
@@ -0,0 +1,22 @@
+// Config for Text
+
+var text = {
+  c: 'color',
+  fF: 'fontFamily',
+  fSz: 'fontSize',
+  fSt: 'fontStyle',
+  fW: 'fontWeight',
+  lH: 'lineHeight',
+  tA: 'textAlign',
+  tDL: 'textDecorationLine',
+  tShC: 'textShadowColor',
+  tShO: 'textShadowOffset',
+  tShR: 'textShadowRadius',
+  tAV: 'textAlignVertical',
+  ltS: 'letterSpacing',
+  tDC: 'textDecorationColor',
+  tDS: 'textDecorationStyle',
+  wrD: 'writingDirection'
+};
+
+module.exports = text;
